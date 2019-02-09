eb41510a0b37158d1e6986ffcb006d194918d4ab
navidmx <navidmx@gmail.com>
2019-02-09 03:20:41 -0500

    IDE changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,22 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
 editor.session.setMode("ace/mode/python");
+
+$(".run-program").click(function () {
+    console.log("Test")
+    runit();
+})
+
+$(document).keydown(function (e) {
+    if (e.which == 17) {
+        isCtrl = true;
+    }
+    if (e.which == 73 && isCtrl) {
+        $(".form-control").focus();
+        startVoice()
+    }
+});
+
+function startVoice() {
+    
+}
