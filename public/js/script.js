fe19027c7314fc555980494e1aa19996fa2ae73f
navidmx <navidmx@gmail.com>
2019-02-09 13:22:34 -0500

    Checking for undefined

--- public/js/script.js
+++ public/js/script.js
@@ -22,14 +22,16 @@ function downloadFile(name) {
 }
 
 function openFile() {
-    
+
 }
 
 function commandEntered(e) {
     if (e.keyCode == 13) {
         $(".form-control").blur();
         editor.focus();
-        runCommand($("#scriptBox").val());
+        if ($("#scriptBox").val() != undefined) {
+            runCommand($("#scriptBox").val());
+        }
         $("#scriptBox")
             .css("color", "#2e9dc6")
             .delay(300)
