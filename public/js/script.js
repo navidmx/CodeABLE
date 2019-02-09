0e899204db9f367b9311dc89b982e98bfedcec5f
navidmx <navidmx@gmail.com>
2019-02-09 11:20:32 -0500

    Added logic file

--- public/js/script.js
+++ public/js/script.js
@@ -2,21 +2,28 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
 editor.session.setMode("ace/mode/python");
 
+promptOpen = false;
+
 $(".run-program").click(function () {
-    console.log("Test")
     runit();
 })
 
+// Keyboard shortcuts
 $(document).keydown(function (e) {
-    if (e.which == 17) {
-        isCtrl = true;
-    }
-    if (e.which == 73 && isCtrl) {
+    // Click "ESC" to switch between command bar and input
+    if (e.which == 27 && ($(".form-control").is(":focus") == false)) {
         $(".form-control").focus();
         startVoice()
+    } else if (e.which == 27) {
+        $(".ace_content").focus();
+        endVoice()
     }
 });
 
 function startVoice() {
+    
+}
+
+function endVoice() {
     
 }
