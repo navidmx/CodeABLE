db14a872b16b21b10572822b145cae314782c1c4
navidmx <navidmx@gmail.com>
2019-02-09 13:36:39 -0500

    Cleaned code

--- public/js/logic.js
+++ public/js/logic.js
@@ -8,7 +8,7 @@ let aceDoc = session.getDocument();
 
 let checkpointNames = [];
 
-for(let i = 0; i < 20; i++)
+for (let i = 0; i < 20; i++)
     editor.insert("test " + (i + 1) + "\n");
 
 editor.gotoLine(10);
@@ -16,36 +16,29 @@ editor.insert("for i in range(5)\n");
 editor.indent();
 editor.insert("x = 1\n");
 
-
 function giveFeedback(text) {
     return text;
 }
 
-
 function runCommand(command) {
-    if(command.includes("run"))
-    {
+    if (command.includes("run")) {
         runit();
-    }
-    else if(command.includes("go to")) {
+    } else if (command.includes("go to")) {
         commandGoTo(command);
-    }
-    else if(command.includes("read")) {
+    } else if (command.includes("read")) {
         commandRead(command);
-    }
-    else if(command.includes("new") || command.includes("make")) {
+    } else if (command.includes("new") || command.includes("make")) {
         commandMake(command);
-    }
-    else if(command.includes("save")) {
+    } else if (command.includes("save")) {
         commandSaveFile(command)
     }
 }
 
 //saves a file, given the name
 function commandSaveFile(command) {
-    if(command.includes("as")) {
+    if (command.includes("as")) {
         fileName = command.substring(command.indexOf("as") + 3, command.length);
-        if(fileName.includes(".py")) {
+        if (fileName.includes(".py")) {
             fileName = fileName.split(".py")[0]
         }
         downloadFile(fileName)
@@ -56,24 +49,20 @@ function commandSaveFile(command) {
 
 //figures out where to go, given the string command
 function commandGoTo(command) {
-    if(command.includes("line")) {
+    if (command.includes("line")) {
 
         let lineNum = getLineFromCommand(command);
 
-        if(lineNum >= 0) {
+        if (lineNum >= 0) {
 
-            if(command.includes("end")) {
+            if (command.includes("end")) {
                 goToLine(lineNum, 1);
-            }
-
-            else
+            } else
                 goToLine(lineNum, 0);
         }
-    }
-    else if(command.includes("loop") || command.includes("checkpoint")) {
+    } else if (command.includes("loop") || command.includes("checkpoint")) {
         goToCheckpoint(command);
-    }
-    else if(command.includes("next")) {
+    } else if (command.includes("next")) {
         goToObject(command);
     }
 }
@@ -81,13 +70,13 @@ function commandGoTo(command) {
 //goes to specific line
 //loc 0 = start, loc 1 = end
 function goToLine(lineNum, loc) {
-    if(loc == 0) {
+    if (loc == 0) {
         editor.insert("what");
         editor.gotoLine(lineNum);
     }
     //goes to line below and then goes 
     //to the left once (to go to end of prev line)
-    else if(loc == 1) {
+    else if (loc == 1) {
         let lastLine = editor.session.getLength();
 
         editor.gotoLine(lineNum + 1);
@@ -95,11 +84,10 @@ function goToLine(lineNum, loc) {
     }
 }
 
-
 function getLineFromCommand(command) {
     let index = 0;
 
-    if(command.length > command.indexOf("line") + 4) {
+    if (command.length > command.indexOf("line") + 4) {
         index = command.indexOf("line") + 5;
     }
 
@@ -107,7 +95,7 @@ function getLineFromCommand(command) {
 
     let lastLine = editor.session.getLength();
 
-    if(lineNum > lastLine) {
+    if (lineNum > lastLine) {
         giveFeedback("Line " + lineNum.toString() + " does not exist. Last line is " + lastLine.toString());
         return -1;
     }
@@ -115,42 +103,39 @@ function getLineFromCommand(command) {
     return lineNum;
 }
 
-
 function goToObject(command) {
-    if(command.includes("loop")) {
+    if (command.includes("loop")) {
 
         //if user mentions a checkpoint, goes to it
-        for(let name in checkpointNames) {
-            if(command.includes(name)) {
+        for (let name in checkpointNames) {
+            if (command.includes(name)) {
                 giveFeedback("Going to loop checkpoint " + name);
                 goToCheckpoint("loop", name);
             }
         }
 
         //otherwise, goes to next for loop
-        if(command.includes("for")) {
+        if (command.includes("for")) {
             let line = editor.findNext(" for ").startRow;
             let col = editor.findNext(" for ").startColumn;
 
             editor.insert(line.toString() + "\n" + col.toString());
-        }
-        else if(command.includes("while")) {
+        } else if (command.includes("while")) {
             //TODO
         }
     }
 }
 
-
 function commandRead(command) {
-    if(command.includes("line")) {
+    if (command.includes("line")) {
         //TODO
     }
 }
 
 function commandMake(command) {
-    if(command.includes("checkpoint")) {
+    if (command.includes("checkpoint")) {
         let index = command.indexOf("checkpoint");
-        if(command.length > index + 10) {
+        if (command.length > index + 10) {
 
             let line = editor.getCursorPosition().row + 1;
 
@@ -159,8 +144,7 @@ function commandMake(command) {
     }
 }
 
-
-function read(read_range){
+function read(read_range) {
     return aceDoc.getTextRange(read_range);
 }
 
@@ -168,7 +152,7 @@ function makeCheckpoint(type, name, line) {
     goToLine(line, 1);
     let cursorPosition = editor.getCursorPosition();
     let symbol = " ~ ";
-    let comment = "#" + symbol + type + ": \"" + name + "\""; 
+    let comment = "#" + symbol + type + ": \"" + name + "\"";
 
     session.insert(cursorPosition, comment);
 }
@@ -179,9 +163,8 @@ function goToCheckpoint(type, name) {
     let symbol = " ~ ";
     let comment = "#" + symbol + type + ": \"" + name + "\"";
 
-    for(let i = 0; i < allLines.length; i++){
-        if(allLines[i].includes(comment))
-        {
+    for (let i = 0; i < allLines.length; i++) {
+        if (allLines[i].includes(comment)) {
             goToLine(i + 2);
             giveFeedback("Now at " + type + " " + name);
         }
