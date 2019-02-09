a9c26a268216e564d2599f4c614ecaa55bab1d3e
Gabriel Rasskin <43894452+grasskin@users.noreply.github.com>
2019-02-09 12:10:54 -0500

    runcommand call

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,54 +1,45 @@
-var editor = ace.edit("editor");
+var editor = ace.edit('editor');
 var session = editor.getSession();
 var aceDoc = session.getDocument();
 
 var checkpointNames = [];
 
-for(let i = 0; i < 20; i++)
-    editor.insert("test " + (i + 1) + "\n");
+for (let i = 0; i < 20; i++) editor.insert('test ' + (i + 1) + '\n');
 
-editor.insert(" for ");
-
+editor.insert(' for ');
 
 console.log(read(1, 5, 2, 4));
 
-
-runCommand("go to next for loop");
+//runCommand('go to next for loop');
 
 function runCommand(command) {
-
+    console.log('command' + command);
     command.trim();
     command.toLowerCase();
 
-    if(command.includes("go to")) {
+    if (command.includes('go to')) {
         voiceGoTo(command);
-    }
-    else if(command.includes("read")) {
+    } else if (command.includes('read')) {
         voiceRead(command);
-    }
-    else if(command.includes("new")) {
+    } else if (command.includes('new')) {
         voiceMakeNew(command);
     }
 }
 
 //figures out where to go, given the string command
 function voiceGoTo(command) {
-    if(command.includes("line")) {
-
+    if (command.includes('line')) {
         lineNum = getLineFromCommand(command);
 
-        if(lineNum >= 0)
-
-        if(command.includes("end")) {
-            goToLine(lineNum, 1);
-        }
+        if (lineNum >= 0)
+            if (command.includes('end')) {
+                goToLine(lineNum, 1);
+            }
 
         goToLine(lineNum, 0);
-    }
-    else if(command.includes("loop") || command.includes("checkpoint")) {
+    } else if (command.includes('loop') || command.includes('checkpoint')) {
         goToCheckpoint(command);
-    }
-    else if(command.includes("next")) {
+    } else if (command.includes('next')) {
         goToObject(command);
     }
 }
@@ -56,72 +47,62 @@ function voiceGoTo(command) {
 //goes to specific line
 //loc 0 = start, loc 1 = end
 function goToLine(lineNum, loc) {
-    if(loc == 0) {
+    if (loc == 0) {
         editor.gotoLine(lineNum);
     }
-    //goes to line below and then goes 
+    //goes to line below and then goes
     //to the left once (to go to end of prev line)
-    else if(loc == 1) {
+    else if (loc == 1) {
         editor.gotoLine(lineNum + 1);
         editor.navigateLeft(1);
     }
 }
 
-
 function goToCheckpoint(command) {
     //TODO
 }
 
-
 function goToObject(command) {
-    if(command.includes("loop")) {
-
+    if (command.includes('loop')) {
         //if user mentions a checkpoint, goes to it
-        for(let name in checkpointNames) {
-            if(command.includes(name)) {
+        for (let name in checkpointNames) {
+            if (command.includes(name)) {
                 goToCheckpoint(name);
             }
         }
 
         //otherwise, goes to next loop
-        if(command.includes("for")) {
-            let line = editor.findNext(" for ").startRow;
-            let col = editor.findNext(" for ").startColumn;
+        if (command.includes('for')) {
+            let line = editor.findNext(' for ').startRow;
+            let col = editor.findNext(' for ').startColumn;
 
-            editor.insert(line.toString() + "\n" + col.toString());
-        }
-        else if(command.includes("while")) {
+            editor.insert(line.toString() + '\n' + col.toString());
+        } else if (command.includes('while')) {
             //TODO
         }
     }
 }
 
-
 function voiceRead(command) {
-
     //TODO: this is ambiguous, user could say "read lines 5 to 7" and it would
     //just read line 5
-    if(command.includes("line")) {
-        
+    if (command.includes('line')) {
         let lineNum = getLineFromCommand(command);
-
-        
     }
 }
-
 
 function getLineFromCommand(command) {
     let index = 0;
 
-    if(command.length > command.indexOf("line") + 4) {
-        index = command.indexOf("line") + 5;
+    if (command.length > command.indexOf('line') + 4) {
+        index = command.indexOf('line') + 5;
     }
 
     let lineNum = parseInt(command.substring(index, command.length));
 
     let lastLine = editor.session.getLength();
 
-    if(lineNum > lastLine) {
+    if (lineNum > lastLine) {
         //TODO: say "line __ does not exist. Last line is __."
         return -1;
     }
@@ -129,9 +110,7 @@ function getLineFromCommand(command) {
     return lineNum;
 }
 
-
-function read(from_line, from_col, to_line, to_col)
-{
-   // const read_range = new Range(from_line, from_col, to_line, to_col)
-   // return aceDoc.getTextRange(read_range);
+function read(from_line, from_col, to_line, to_col) {
+    // const read_range = new Range(from_line, from_col, to_line, to_col)
+    // return aceDoc.getTextRange(read_range);
 }
--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,28 @@
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/twilight");
-editor.session.setMode("ace/mode/python");
+var editor = ace.edit('editor');
+editor.setTheme('ace/theme/twilight');
+editor.session.setMode('ace/mode/python');
 var session = editor.getSession();
 var aceDoc = session.getDocument();
 
-$(".run-program").click(function () {
+$('.run-program').click(function() {
     runit();
-})
+});
 
 // Keyboard shortcuts
-$(document).keydown(function (e) {
+$(document).keydown(function(e) {
     // Click "ESC" to switch between command bar and input
-    if (e.which == 27 && ($(".form-control").is(":focus") == false)) {
-        $(".form-control").focus();
-        startVoice()
+    if (e.which == 27 && $('.form-control').is(':focus') == false) {
+        $('.form-control').focus();
+        startVoice();
     } else if (e.which == 27) {
-        $(".ace_content").focus();
-        endVoice()
+        $('.ace_content').focus();
+        endVoice();
     }
 });
 
 function commandEntered(e) {
     if (e.keyCode == 13) {
-        runCommand($("#scriptBox").val());
+        runCommand($('#scriptBox').val());
         return false;
     }
-}
-
-function startVoice() {
-    // Start Azure's voice detection, enter in runCommand(voice)
-}
-
-
-function endVoice() {
-    // Halt voice input
 }
