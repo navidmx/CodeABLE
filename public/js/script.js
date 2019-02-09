cbe7cf19eefde9f099b1b83c394dd504b4fcc740
navidmx <navidmx@gmail.com>
2019-02-09 12:20:47 -0500

    Styled command input

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
-$('.run-program').click(function() {
+$('.run-program').click(function () {
     runit();
 });
 
 // Keyboard shortcuts
-$(document).keydown(function(e) {
+$(document).keydown(function (e) {
     // Click "ESC" to switch between command bar and input
     if (e.which == 27 && $('.form-control').is(':focus') == false) {
         $('.form-control').focus();
@@ -19,7 +19,19 @@ function commandEntered(e) {
         $(".form-control").blur();
         $(".ace_content").focus();
         runCommand($("#scriptBox").val());
-        $("#scriptBox").val("");
+        $("#scriptBox")
+            .css("color", "#2e9dc6")
+            .delay(300)
+            .queue(function (next) {
+                $(this).css("color", "rgba(255, 255, 255, 0)");
+                next();
+            })
+            .delay(1000)
+            .queue(function (next) {
+                $(this).val("");
+                $(this).css("color", "white");
+                next();
+            })
         return false;
     }
 }
